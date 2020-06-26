f5e70c12b814dced452e2c0ce88da7fe7136a615
Jiwon Park <jiwon@Jiwons-MacBook-Pro.local>
2020-06-26 11:54:52 +0200

    modified boolean parser with tuples

--- boolean_parser.py
+++ boolean_parser.py
@@ -7,25 +7,15 @@ import ply.yacc as yacc
 import sys
 
 tokens = (
-    'TRUE',
-    'FALSE',
-    'OR',
-    'AND',
-    'NOT',
-    'XOR'
+    'TRUE', 'FALSE',
+    'OR', 'AND', 'NOT',
 )
 
 t_TRUE = r'True'
-
 t_FALSE = r'False'
-
 t_OR = r'[|]'
-
 t_AND = r'&'
-
 t_NOT = r'!'
-
-t_XOR = r'\^'
 
 t_ignore = r' '
 
@@ -36,7 +26,6 @@ lex.lex()
 
 precedence = (
     ('left','OR'),
-    ('left', 'XOR'),
     ('left','AND'),
     ('left','NOT')
 )
@@ -51,20 +40,18 @@ def p_string(p):
         p[0] = []
     else:
         p[0] = p[1]
-    #uncomment for to test on terminal
-    #print(p[0])
+    #uncomment to test on terminal
+    print(p[0])
 
 def p_expression(p):
     """
-    expression : expression OR expression
+    expression : NOT expression
+               | expression OR expression
                | expression AND expression
-               | expression XOR expression
-               | NOT expression
     """
-    if p[1] == '!':  p[0] = not p[2]
-    if p[2] == '|': p[0] = p[1] or p[3]
-    elif p[2] == '&':   p[0] = p[1] and p[3]
-    elif p[2] == '^':   p[0] = p[1] ^ p[3]
+    if p[1] == '!':  p[0] = ('not', p[2])
+    if p[2] == '|': p[0] = ('or', p[1], p[3])
+    elif p[2] == '&':   p[0] = ('and', p[1], p[3])
 
 def p_value(p):
     """
@@ -81,33 +68,10 @@ def p_error(p):
 
 parser = yacc.yacc()
 
-# uncomment below to test on the terminal
-"""
+#uncomment below to test on the terminal
 while True:
     try:
         s = input('')
     except EOFError:
         break
     parser.parse(s)
-"""
-
-def assert_raises(exc, f, *args):
-    try:
-        f(*args)
-    except exc:
-        pass
-    else:
-        raise AssertionError("Expected %r" % (exc,))
-
-def test():
-    assert parser.parse("False & True | False") == False
-    assert parser.parse("False & False & False | True") == True
-    assert parser.parse("") == []
-    assert parser.parse("True^False|True") == True
-    assert_raises(TypeError, parser.parse, "Blah")
-    assert_raises(TypeError, parser.parse, "10")
-    assert_raises(TypeError, parser.parse, "1C")
-
-if __name__ == "__main__":
-    test()
-    print ("All tests passed.")
