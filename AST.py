66449b98a41b66fe68054730cbfb4c358b836fbd
jiwonparc <56113579+jiwonparc@users.noreply.github.com>
2020-07-23 17:32:24 +0200

    updated with visitor pattern

--- AST.py
+++ AST.py
@@ -1,3 +1,5 @@
+# defining a class for AST
+
 class SyntaxInfo(object):
     """ Store the low level info of the node, like the line number and column """
     def __init__(self, line_number, column):
@@ -53,10 +55,109 @@ class Assignment(object):
     def __str__(self):
         return "Assignment({},{},{})".format(self.name, self.value, self.syntax_info)
 
-
-class HybridModel(object):
-    def __init__(self, expressions):
-        self.expressions = expressions
+class Bracket(object):
+    """ representation of brackets around the node """
+    def __init__(self, type, expression, syntax_info):
+        self.type = type
+        self.expression = expression
+        self.syntax_info = syntax_info
 
     def __str__(self):
-        return "HybridModel({})".format(self.expressions)
+        return "Bracket({},{})".format(self.type, self.expression)
+
+class Modality(object):
+    """ representation of an Modality node of the AST """
+    def __init__(self, type, program, formula, syntax_info):
+        self.type = type
+        self.program = program
+        self.formula = formula
+        self.syntax_info = syntax_info
+
+    def __str__(self):
+        return "Modality({},{},{})".format(self.type, self.program, self.formula)
+
+class Quantifier(object):
+    """ representation of a quantifier node of the AST """
+    def __init__(self, type, variable, formula, syntax_info):
+        self.type = type
+        self.variable = variable
+        self.formula = formula
+        self.syntax_info = syntax_info
+
+    def __str__(self):
+
+        return "Quantifier({},{},{})".format(self.name, self.value, self.syntax_info)
+
+class Tree(object):
+    def __init__(self, programs):
+        self.programs = programs
+
+    def __str__(self):
+        return "Tree({})".format(self.programs)
+
+# visitor pattern for the tree
+# code from https://chris-lamb.co.uk/posts/visitor-pattern-in-python
+class Visitor(object):
+    @dispatch.on('node')
+    def visit(self, node):
+        """This is the generic method"""
+
+    @visit.when(Tree)
+    def visit(self, node):
+        map(self.visit, node.programs)
+
+    @visit.when(Quantifier)
+    def visit(self, node):
+        print("{} {} {}".format(node.type, node.variable, self.visit(node.formula)))
+
+    @visit.when(Modality)
+    def visit(self, node):
+        if node.type == 'BOX':
+            print("[{}] {}".format(node.program, node.formula))
+        else:
+            print("<{}> {}".format(node.program, node.formula))
+
+    @visit.when(Bracket)
+    def visit(self, node):
+        if node.type == 'PAREN':
+            print ("({})".format(self.visit(node.expression)))
+        else:
+            print("{"+self.visit(node.expression)+"}")
+
+    @visit.when(Assignment)
+    def visit(self, node):
+        if node.value == 'NONDASSIGNMENT':
+            print("{} := *;".format(node.name))
+        else:
+            print("{} := {};".format(node.name, node.value))
+
+    @visit.when(Function)
+    def visit(self, node):
+        if node.type == 'CST_SYM':
+            print(node.name + "()")
+        else:
+            print("{}({})".format(node.name, self.visit(node.variable)))
+
+    @visit.when(Operator)
+    def visit(self, node):
+        if node.type == 'REPETITION':
+            print("{"+self.visit(node.left)+"}*")
+        elif node.type == 'C_EVOLOUTION':
+            print("{"+ self.visit(node.left) + " & "+ self.visit(node.right)+"}")
+        elif node.type == 'CHOICE':
+            print("{} ++ {}".format(self.visit(node.left), self.visit(node.right)))
+        elif node.type == 'TEST':
+            print("?{};".format(self.visit(node.left)))
+        elif node.type == 'DIFFERENTIAL':
+            if node.right != None:
+                print("{} '= {}".format(self.visit(node.left), self.visit(node.right)))
+            else:
+                print(self.visit(node.left)+"'")
+        elif node.type == '!':
+            print("!{}".fomrat(self.visit(node.left)))
+        else:
+            print("{} {} {}".fomrat(self.visit(node.left), node.type, self.visit(node.right)))
+
+    @visit.when(Variable)
+    def visit(self, node):
+        print (self.value)
